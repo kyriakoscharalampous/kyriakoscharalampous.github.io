0ac6badd487f54d4c8fbb3bfaabf1d3704c7c537
kyriakoscharalampous <k.charala@kyriakoscharalampous.com>
2026-04-15 15:03:34 +0000

    Deploying to gh-pages from @ kyriakoscharalampous/kyriakoscharalampous.github.io@bb70ef579103687c6f38b8dfefde71c20ca964ac 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Download my full CV or browse the summary below.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
